33b89fdc118fa527436e8611947cbd2863c3a023
test <a@b.com>
2015-08-13 12:40:14 -0500

    Se agrego el funcionamiento de la calculadora en el main.c

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
-#include <calculadora.h>
+#include "calculadora.h"
 
 int main(void){
+char op;
+int a,b,r;
+printf("Que operacion desea realizar?\n<a>-Suma\n<b>-Resta\n<c>-Multiplicacion\n<d>-Division\n");
+scanf("%c",&op);
+printf("Ingrese el primer valor");
+scanf("%d",&a);
+printf("Ingrese el segundo valor");
+scanf("%d",&b);
+switch(op){
+	case 'a':
+	r=suma(a,b);
+	break;
+	case 'b':
+	r=resta(a,b);
+	break;
+	case 'c':
+	r=multiplicacion(a,b);
+	break;
+	case 'd':
+	r=division(a,b);
+	break;
+}
+printf("\nEl resultado es: %d",r);
 return 0;
 }
