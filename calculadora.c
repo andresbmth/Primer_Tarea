fdbb78fd3e2b911b78399e5351f313e13a29639a
test <a@b.com>
2015-08-13 12:14:57 -0500

    Se escribieron las funciones en calculadora.c

---
+++ calculadora.c
@@ -0,0 +1,17 @@
+#include "calculadora.h"
+
+int suma(int a,int b){
+	return a+b;
+}
+
+int resta(int a,int b){
+	reutrn a-b;
+}
+
+int multiplicacion(int a,int b){
+	return a*b;
+}
+
+int division(int a,int b){
+	return a/b;
+}
